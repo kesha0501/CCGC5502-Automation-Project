8a83d3a373a4389370dada53503fccbebd679adc
kesha0501 <keshashah1666@gmail.com>
2025-08-06 12:10:57 -0400

    Updated load balancer to Standard SKU to fix VmIsNotInSameAvailabilitySetAsLb error

--- terraform/modules/loadbalancer/main.tf
+++ terraform/modules/loadbalancer/main.tf
@@ -3,32 +3,30 @@ resource "azurerm_public_ip" "lb_ip" {
   location            = var.location
   resource_group_name = var.resource_group
   allocation_method   = "Static"
+  sku                 = "Standard"
   tags                = var.tags
 }
-
 resource "azurerm_lb" "lb" {
   name                = "6553-lb"
   location            = var.location
   resource_group_name = var.resource_group
+  sku                 = "Standard"
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.lb_ip.id
   }
   tags = var.tags
 }
-
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
   loadbalancer_id = azurerm_lb.lb.id
   name            = "6553-backend"
 }
-
 resource "azurerm_network_interface_backend_address_pool_association" "nic_pool" {
   count                   = length(var.vm_nic_ids)
   network_interface_id    = var.vm_nic_ids[count.index]
   ip_configuration_name   = "internal"
   backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
 }
-
 resource "azurerm_lb_probe" "probe" {
   loadbalancer_id = azurerm_lb.lb.id
   name            = "6553-http-probe"
@@ -36,7 +34,6 @@ resource "azurerm_lb_probe" "probe" {
   port            = 80
   request_path    = "/"
 }
-
 resource "azurerm_lb_rule" "rule" {
   loadbalancer_id                = azurerm_lb.lb.id
   name                           = "6553-http-rule"
