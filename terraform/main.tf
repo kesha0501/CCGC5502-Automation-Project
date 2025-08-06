4fc3623d5fb9a157fad2f33c633acae41a203688
kesha0501 <keshashah1666@gmail.com>
2025-08-06 15:26:20 -0400

    Fixed duplicate provider, resource, module, and output definitions in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,49 +14,6 @@ module "network" {
 }
 
 module "vms" {
-  source              = "./modules/vms"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  subnet_id           = module.network.subnet_id
-  admin_username      = "adminuser"
-  ssh_public_key      = file("~/.ssh/id_rsa.pub")
-}
-
-module "loadbalancer" {
-  source         = "./modules/loadbalancer"
-  resource_group = azurerm_resource_group.rg.name
-  location       = var.location
-  humber_id      = "6553"
-  tags           = {
-    environment = "dev"
-    project     = "CCGC5502"
-  }
-  vm_nic_ids     = module.vms.nic_ids
-}
-
-output "vm_public_ips" {
-  value = module.vms.public_ips
-}
-
-output "load_balancer_fqdn" {
-  value = module.loadbalancer.lb_fqdn
-}
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-6553"
-  location = var.location
-}
-
-module "network" {
-  source              = "./modules/network"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-}
-
-module "vms" {
   source         = "./modules/vms"
   resource_group = azurerm_resource_group.rg.name
   location       = var.location
