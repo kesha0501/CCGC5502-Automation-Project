9e467ae6029f3bfc40e2516d72df396831f774ac
kesha0501 <keshashah1666@gmail.com>
2025-08-06 20:54:47 -0400

    Fixed vms module argument errors by correcting variable name from n01736553 to humber_id

--- terraform/modules/vms/main.tf
+++ terraform/modules/vms/main.tf
@@ -1,6 +1,16 @@
+resource "azurerm_public_ip" "pip" {
+  count               = 3
+  name                = "${var.humber_id}-pip${count.index + 1}"
+  location            = var.location
+  resource_group_name = var.resource_group
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
 resource "azurerm_network_interface" "nic" {
   count               = 3
-  name                = "${var.n01736553}-nic${count.index + 1}"
+  name                = "${var.humber_id}-nic${count.index + 1}"
   location            = var.location
   resource_group_name = var.resource_group
   tags                = var.tags
@@ -13,35 +23,27 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_public_ip" "pip" {
-  count               = 3
-  name                = "${var.n01736553}-pip${count.index + 1}"
-  location            = var.location
-  resource_group_name = var.resource_group
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
-
 resource "azurerm_managed_disk" "data_disk" {
-  count               = 3
-  name                = "${var.n01736553}-vm${count.index + 1}-datadisk"
-  location            = var.location
-  resource_group_name = var.resource_group
+  count                = 3
+  name                 = "${var.humber_id}-vm${count.index + 1}-datadisk"
+  location             = var.location
+  resource_group_name  = var.resource_group
   storage_account_type = "Standard_LRS"
-  create_option       = "Empty"
-  disk_size_gb        = 10
-  tags                = var.tags
+  create_option        = "Empty"
+  disk_size_gb         = 10
+  tags                 = var.tags
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
   count               = 3
-  name                = "${var.n01736553}-vm${count.index + 1}"
+  name                = "${var.humber_id}-vm${count.index + 1}"
   location            = var.location
   resource_group_name = var.resource_group
   size                = "Standard_B1ms"
   admin_username      = "adminuser"
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
   tags                = var.tags
 
   admin_ssh_key {
@@ -70,10 +72,10 @@ resource "azurerm_virtual_machine_data_disk_attachment" "data_disk_attachment" {
   caching            = "ReadWrite"
 }
 
-output "nic_ids" {
-  value = azurerm_network_interface.nic[*].id
-}
-
 output "public_ips" {
   value = azurerm_public_ip.pip[*].ip_address
+}
+
+output "nic_ids" {
+  value = azurerm_network_interface.nic[*].id
 }
--- terraform/modules/vms/variables.tf
+++ terraform/modules/vms/variables.tf
@@ -1,19 +1,25 @@
+variable "humber_id" {
+  type        = string
+  description = "Humber ID for naming resources"
+}
+
 variable "resource_group" {
-  type = string
+  type        = string
+  description = "Name of the resource group"
 }
 
 variable "location" {
-  type = string
-}
-
-variable "subnet_id" {
-  type = string
-}
-
-variable "n01736553" {
-  type = string
+  type        = string
+  description = "Azure region for resources"
 }
 
 variable "tags" {
-  type = map(string)
+  type        = map(string)
+  description = "Tags for resources"
+  default     = {}
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID of the subnet"
 }
