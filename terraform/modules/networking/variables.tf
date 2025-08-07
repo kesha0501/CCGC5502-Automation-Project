dcee4fdf9a44de914dbbd287468e716e58cca201
kesha0501 <keshashah1666@gmail.com>
2025-08-06 20:43:57 -0400

    Added variables.tf to networking module to fix undeclared variable errors

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -1,6 +1,20 @@
-variable "resource_group_name" {
-  type = string
+variable "humber_id" {
+  type    = string
+  description = "Humber ID for naming resources"
 }
+
+variable "resource_group" {
+  type    = string
+  description = "Name of the resource group"
+}
+
 variable "location" {
-  type = string
+  type    = string
+  description = "Azure region for resources"
+}
+
+variable "tags" {
+  type    = map(string)
+  description = "Tags for resources"
+  default = {}
 }
